dda44b7f4875ed80276eee445417da3268978d08
geekbrains <info@geekbrains.ru>
2022-10-25 21:22:17 +0300

    Task 3

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -2,10 +2,21 @@ package com.max.idea;
 
 public class Main {
     public static void main(String[] args) {
+        // Task 1
         System.out.println("Hello World!");
+
+        // Task 2
         float a = 123.456f;
         byte b = 127;
         System.out.println(a);
         System.out.println(b);
+
+        // Task 3
+        int [] newArray = {1, 4, 6, 12, 5};
+        int last = newArray[0];
+        newArray[0] = newArray[4];
+        newArray[4] = last;
+
+        System.out.println(newArray[0] + newArray[2]);
     }
 }
