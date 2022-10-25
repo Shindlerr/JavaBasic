532b41eac1ec9db6d807693737aaeb7ae363e64c
geekbrains <info@geekbrains.ru>
2022-10-25 19:58:46 +0300

    Task 2

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -3,6 +3,9 @@ package com.max.idea;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        float a = 123.456f;
+        byte b = 127;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
