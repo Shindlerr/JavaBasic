00706d947266474baf1a097e852dfe99faebe411
geekbrains <info@geekbrains.ru>
2022-10-29 17:12:18 +0300

    Task 5

--- src/Task_5/Task_5.java
+++ src/Task_5/Task_5.java
@@ -1,7 +1,25 @@
 package Task_5;
 
+import java.util.Scanner;
+
 public class Task_5 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        String res = sc.next();
+        switch (res) {
+            case "+": System.out.println(a + b);
+                    break;
+            case "-": System.out.println(a - b);
+                    break;
+            case "*": System.out.println(a * b);
+                    break;
+            case "/": System.out.println(a / b);
+                    break;
+        }
 
     }
 }
