c02b59500da3b0f107b7274bb245d45e274bda99
geekbrains <info@geekbrains.ru>
2022-11-05 14:45:29 +0300

    Task 13

--- src/Task_13/Task_13.java
+++ src/Task_13/Task_13.java
@@ -6,20 +6,23 @@ public class Task_13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
+        // "I love java 8 Я люблю java 14 core1" 0061 - 007A
         String allWords = sc.nextLine();
+        char[] letArray;
+        String[] newArray = allWords.toLowerCase().split(" ");
+        int count = 0;
 
-        // "I love java 8 Я люблю java 14 core1"
-
-        String[] newArray = allWords.split(" ");
-
-        /* for (int i = 0; i < newArray.length; i++) {
-            System.out.println(newArray[i]);
-        }
         for (int i = 0; i < newArray.length; i++) {
-            int result;
-            result = newArray[i].compareTo(i);
-        }*/
-
+            letArray = newArray[i].toCharArray();
+            for (int j = 0; j < letArray.length; j++) {
+                if (letArray[j] < '\u0061' || letArray[j] > '\u007A') {
+                    break;
+                } else if (j == letArray.length - 1) {
+                    System.out.println(newArray[i]);
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
     }
 }
